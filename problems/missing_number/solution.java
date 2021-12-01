7f544125c1486f4368877a5ac881411629c909c4
Khizer Khan <60915859+khizerkhan-2316@users.noreply.github.com>
2021-12-01 23:14:39 +0000

    Sync LeetCode submission - Missing Number (java)

---
+++ problems/missing_number/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+      
+       Arrays.sort(nums);
+        
+          if(nums[0] == 1){
+            return 0;
+        }
+        
+        
+              
+        for(int i = 0; i < nums.length; i++){
+            
+            int currentNumber = nums[i]; 
+            
+            
+            if(i + 1 < nums.length){
+                
+                if(!(nums[i + 1] == currentNumber + 1)){
+                    
+                    System.out.println("True");
+                    return currentNumber + 1;
+                } 
+            } else if(i == nums.length - 1){
+                return currentNumber + 1;
+            }
+        }
+        
+       
+        return 1;
+        
+    }
+}
